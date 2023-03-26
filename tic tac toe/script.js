61f8681a71beae6328d41f9439783a6e79136f25
usamafse <126562762+usamafse@users.noreply.github.com>
2023-03-26 23:12:38 +0500

    Update script.js
    I will add the python code of tic tac toe game logic at the end of the file.

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -65,3 +65,70 @@ reset.addEventListener('click', ()=>{
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
 })
 
+
+
+
+
+// The logic for a Tic Tac Toe game can be implemented using a 3x3 matrix (or array) to represent the game board. 
+// The matrix can contain three different values: 'X' for the first player, 'O' for the second player, and '-' for an empty cell on the board. 
+// The game starts with an empty board, and players take turns to place their symbol ('X' or 'O') in an empty cell until one player wins or the game ends in a draw.
+
+// Here is an example of the game logic using Python:
+/* # Initialize the game board with empty cells
+board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+]
+
+# Define a function to print the current state of the board
+def print_board():
+    for row in board:
+        print(" ".join(row))
+    print()
+
+# Define a function to check if a player has won
+def check_win(player):
+    # Check rows
+    for row in board:
+        if row.count(player) == 3:
+            return True
+    # Check columns
+    for col in range(3):
+        if board[0][col] == player and board[1][col] == player and board[2][col] == player:
+            return True
+    # Check diagonals
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return True
+    if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return True
+    return False
+
+# Define a function to check if the game is a draw
+def check_draw():
+    for row in board:
+        if "-" in row:
+            return False
+    return True
+
+# Start the game loop
+player = 'X'
+while True:
+    print_board()
+    print("Player", player, "turn")
+    row = int(input("Enter row (1-3): ")) - 1
+    col = int(input("Enter column (1-3): ")) - 1
+    if board[row][col] != "-":
+        print("Cell already occupied")
+        continue
+    board[row][col] = player
+    if check_win(player):
+        print_board()
+        print("Player", player, "wins!")
+        break
+    elif check_draw():
+        print_board()
+        print("Game ends in a draw")
+        break
+    player = 'O' if player == 'X' else 'X'
+*/
